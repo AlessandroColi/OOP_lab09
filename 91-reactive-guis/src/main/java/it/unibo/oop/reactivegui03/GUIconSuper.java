6aaeded25e6dabbd523b937793a4ddfc01907e65
Alessandro Coli - alessandro.coli2@studio.unibo.it <alessandro.coli2@studio.unibo.it>
2022-11-21 11:40:01 +0100

    aggiunta classe con super a 91-3 stavolta davvero

---
+++ 91-reactive-guis/src/main/java/it/unibo/oop/reactivegui03/GUIconSuper.java
@@ -0,0 +1,39 @@
+package it.unibo.oop.reactivegui03;
+
+import it.unibo.oop.reactivegui02.ConcurrentGUI;
+
+public class GUIconSuper extends ConcurrentGUI {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	GUIconSuper(){
+		super();
+		Control ctrl = new Control();
+	}
+	
+private class Control implements Runnable{
+    	
+    	private boolean basta = false;
+        
+        public void run() {
+            while (!this.basta ) {   
+                    try {
+						Thread.sleep(10000);
+	                    this.stopCounting();
+	                    
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+                    
+            }
+        }
+		public void stopCounting() {
+        	this.basta=true;
+        	stop();
+		}
+    	
+    }
+}
